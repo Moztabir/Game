147579127f3974d3915f594daf81d2fc3981dcd5
Moztabir <88281794+Moztabir@users.noreply.github.com>
2026-03-13 20:06:50 -0400

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,13 +1,8 @@
-const supabaseUrl = "https://ktlarcejrqsovffqhfcu.supabase.co";
-const supabaseKey = "sb_publishable_ndfyElH6tRakFhd07AEPfg_PVjOiHYu";
-
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
-
 async function submitScore(gameNumber, score){
   const playerId = localStorage.getItem("mq_player_id");
 
   if(!playerId){
-    console.warn("No player ID found");
+    console.warn("No player ID found. Score not submitted.");
     return;
   }
 
